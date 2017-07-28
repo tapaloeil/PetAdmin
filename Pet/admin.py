44232543e73dbbe2a0cfefb5781ae185a99e686c
ARESP <sylvain.hebert@gmail.com>
2017-07-28 23:37:14 +0200

    init

---
+++ Pet/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import PetModel, PetImageModel
+from jet.admin import CompactInline
+# Register your models here.
+
+class PetImageModelAdmin(CompactInline):
+	model=PetImageModel
+
+class PetModelAdmin(admin.ModelAdmin):
+	list_display=("Name","Category","Sex","BirthDt","City","Departement","Status")
+	list_filter=("Category","Sex", "City", "Departement", "Status")
+	fieldsets=(
+		(None,{
+			'fields':('Name', 'Category', 'Sex',('BirthDt'),'City','Departement','OK_CHAT','OK_CHIEN','OK_ENFANT','Status')
+		}),
+		('Description',{
+			'fields':('Description',),
+			}),
+		)
+	inlines=(PetImageModelAdmin,)
+
+admin.site.register(PetModel,PetModelAdmin)
